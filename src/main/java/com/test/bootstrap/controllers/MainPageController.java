dcfce10fbc9f56e12eac3ddac9da7a0040dbd0be
Lord Nighton <lordnightonnik@gmail.com>
2018-10-31 10:38:54 -0300

    Adding a service for unit tests

--- src/main/java/com/test/bootstrap/controllers/MainPageController.java
+++ src/main/java/com/test/bootstrap/controllers/MainPageController.java
@@ -1,6 +1,9 @@
 package com.test.bootstrap.controllers;
 
+import com.test.bootstrap.services.StringService;
+
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,11 +13,14 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/")
 public class MainPageController {
 
+    @Autowired
+    private StringService stringService;
+
     @GetMapping
     public ModelAndView mainPage() {
         ModelAndView model = new ModelAndView("index");
 
-        model.addObject("phrase", StringUtils.trim("  Hello world!  "));
+        model.addObject("phrase", StringUtils.trim("  Hello world!  ") + " " + stringService.returnString());
 
         return model;
     }
---
+++ src/main/java/com/test/bootstrap/services/StringService.java
@@ -0,0 +1,12 @@
+package com.test.bootstrap.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class StringService {
+
+    public String returnString() {
+        return "LordNighton";
+    }
+
+} 
